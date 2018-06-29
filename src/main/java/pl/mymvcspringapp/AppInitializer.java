39ae093d245bb4fd3bf3a9097d8773babe825f44
eMIKARp <emil.karpowicz@gmail.com>
2018-06-29 16:01:59 +0200

    Final commit

--- src/main/java/pl/mymvcspringapp/AppInitializer.java
+++ src/main/java/pl/mymvcspringapp/AppInitializer.java
@@ -1,25 +1,23 @@
 package pl.mymvcspringapp;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration.Dynamic;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-public class AppInitializer implements WebApplicationInitializer {
+public class AppInitializer extends  AbstractAnnotationConfigDispatcherServletInitializer{
 
 	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-
-		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-		ctx.setServletContext(servletContext);
-		DispatcherServlet dispatcher = new DispatcherServlet(ctx);
-		
-		Dynamic dispatcherConfig = servletContext.addServlet("dispatcher", dispatcher);
-        dispatcherConfig.addMapping("/");	
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
 	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
 
 }
